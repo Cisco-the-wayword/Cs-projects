1e9382240ed958093d59c15f581fb228146bd69d
Cisco. A <dexterhack800@gmail.com>
2023-05-04 03:39:17 -0600

    Add files via upload

---
+++ NumberOfWords/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a sentence: ");
+        string sentence = Console.ReadLine();
+
+        int wordCount = CountWords(sentence);
+
+        Console.WriteLine($"The sentence contains {wordCount} words.");
+    }
+
+    static int CountWords(string sentence)
+    {
+        string[] words = sentence.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+        return words.Length;
+    }
+}
